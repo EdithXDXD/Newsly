37b330571c8f57b377efdc36626c19b3a2a83147
Ruyin Shao <ruyinsha@usc.edu>
2017-11-25 14:33:09 -0800

    remote initialize

---
+++ Newsly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Newsly
+//
+//  Created by Ruyin Shao on 11/19/17.
+//  Copyright © 2017 Ruyin Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end  
+
---
+++ Newsly/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Newsly
+//
+//  Created by Ruyin Shao on 11/19/17.
+//  Copyright © 2017 Ruyin Shao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
